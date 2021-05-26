3d13aa8ad833cea0935e8b5f1d749f56b00eca2f
Julia Melnik <julls.hm.md@gmail.com>
2021-05-26 05:05:15 +0300

    DONE

--- index.js
+++ index.js
@@ -6,7 +6,41 @@ const colors = [
     '#009688',
     '#795548',
   ];
+// REFS
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+// formula
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+// DEFAULT
+let intervalId = null;
+let index;
+// COUNTER
+const counter = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    
+    const currentColorIndex = () => {
+    index = randomIntegerFromInterval(0, colors.length - 1);
+    bodyRef.style.backgroundColor = colors[index];         
+    };
+        
+    intervalId = setInterval(currentColorIndex, 850);
+  },
+        
+  stop() {
+    this.isActive = false;
+    clearInterval(intervalId);
+  },
+};
+
+// LISTENERS
+startBtnRef.addEventListener('click', counter.start.bind(counter));
+stopBtnRef.addEventListener('click', counter.stop.bind(counter));
